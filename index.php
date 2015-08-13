8f7cbf3d2553dea2a3cabc0270a011cae3a83608
jephmann <jephmann@gmail.com>
2015-08-12 22:10:29 -0500

    their completed version files

--- index.php
+++ index.php
@@ -7,11 +7,17 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Aquent Gymnasium: Responsive Web Design (Tutorial Homework)</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <ul>
+            <li><a href="portfolio/">My homework</a></li>
+            <li>Their versions
+                <ul>
+            <li><a href="portfolio/completed/portfolio_final.html">Completed</a></li>
+            <li><a href="portfolio/completed/bootstrap_final.html">Bootstrap</a></li>
+                </ul>
+            </li>
+        </ul>
     </body>
 </html>
